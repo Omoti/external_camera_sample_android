27f47ff2961cf73e7fef3f42fe5dad9534df3a8f
Omoti <guti.tomtom@gmail.com>
2022-11-23 14:35:54 +0900

    feat: show thumbnail image

--- app/src/main/java/com/omoti/externalcamerasample/MainScreen.kt
+++ app/src/main/java/com/omoti/externalcamerasample/MainScreen.kt
@@ -3,11 +3,13 @@ package com.omoti.externalcamerasample
 import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +18,13 @@ import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.omoti.externalcamerasample.ui.theme.ExternalCameraSampleTheme
@@ -26,12 +33,14 @@ import com.omoti.externalcamerasample.ui.theme.ExternalCameraSampleTheme
 fun MainScreen() {
     val context = LocalContext.current
 
+    var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
         onResult = { result ->
-            if (result.resultCode != RESULT_OK) return@rememberLauncherForActivityResult
-
-            // TODO: 撮影後の処理
+            if (result.resultCode == RESULT_OK) {
+                bitmap = result.data?.extras?.get("data") as Bitmap
+            }
         }
     )
 
@@ -47,6 +56,12 @@ fun MainScreen() {
                 launchCameraApp(context = context, launcher)
             }) {
                 Text(text = "Launch Camera App")
+            }
+
+            bitmap?.let {
+                Image(bitmap = it.asImageBitmap(), contentDescription = null)
+
+                Text(text = "width: ${it.width}, height: ${it.height}")
             }
         }
     }
