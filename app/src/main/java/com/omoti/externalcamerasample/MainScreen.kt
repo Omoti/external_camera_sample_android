43325fd0b2a1769061d79789888d6aef63e9b9a1
Omoti <guti.tomtom@gmail.com>
2022-11-23 14:09:43 +0900

    feat: launch camera app

--- app/src/main/java/com/omoti/externalcamerasample/MainScreen.kt
+++ app/src/main/java/com/omoti/externalcamerasample/MainScreen.kt
@@ -1,5 +1,13 @@
 package com.omoti.externalcamerasample
 
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +18,23 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.omoti.externalcamerasample.ui.theme.ExternalCameraSampleTheme
 
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+        onResult = { result ->
+            if (result.resultCode != RESULT_OK) return@rememberLauncherForActivityResult
+
+            // TODO: 撮影後の処理
+        }
+    )
+
     Scaffold { paddingValues ->
         Column(
             verticalArrangement = Arrangement.Center,
@@ -24,11 +44,24 @@ fun MainScreen() {
                 .padding(paddingValues = paddingValues),
         ) {
             Button(onClick = {
-                // TODO : Launch Camera App
+                launchCameraApp(context = context, launcher)
             }) {
                 Text(text = "Launch Camera App")
             }
         }
+    }
+}
+
+/**
+ * カメラアプリ起動
+ */
+private fun launchCameraApp(context: Context, launcher: ActivityResultLauncher<Intent>) {
+    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+    if (takePictureIntent.resolveActivity(context.packageManager) != null) {
+        launcher.launch(takePictureIntent)
+    } else {
+        Toast.makeText(context, "Camera App not found.", Toast.LENGTH_SHORT).show()
     }
 }
 
