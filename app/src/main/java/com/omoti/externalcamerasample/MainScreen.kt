27bc580482ce21fd2b757fed0b410cbbfaf15a99
Omoti <guti.tomtom@gmail.com>
2023-02-06 06:28:43 +0900

    feat: save a picture

--- app/src/main/java/com/omoti/externalcamerasample/MainScreen.kt
+++ app/src/main/java/com/omoti/externalcamerasample/MainScreen.kt
@@ -4,6 +4,8 @@ import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -27,22 +29,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.FileProvider
 import com.omoti.externalcamerasample.ui.theme.ExternalCameraSampleTheme
+import java.io.File
+import java.io.IOException
 
 @Composable
 fun MainScreen() {
     val context = LocalContext.current
 
     var bitmap by remember { mutableStateOf<Bitmap?>(null) }
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
 
-    val launcher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.StartActivityForResult(),
-        onResult = { result ->
-            if (result.resultCode == RESULT_OK) {
-                bitmap = result.data?.extras?.get("data") as Bitmap
-            }
-        }
-    )
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult(),
+            onResult = { result ->
+                if (result.resultCode == RESULT_OK) {
+                    // TODO: 保存したファイルからBitmap取得
+                }
+            })
 
     Scaffold { paddingValues ->
         Column(
@@ -53,7 +58,9 @@ fun MainScreen() {
                 .padding(paddingValues = paddingValues),
         ) {
             Button(onClick = {
-                launchCameraApp(context = context, launcher)
+                imageUri = createImageFileUri(context)?.also {
+                    launchCameraApp(context = context, launcher, it)
+                }
             }) {
                 Text(text = "Launch Camera App")
             }
@@ -70,14 +77,34 @@ fun MainScreen() {
 /**
  * カメラアプリ起動
  */
-private fun launchCameraApp(context: Context, launcher: ActivityResultLauncher<Intent>) {
+private fun launchCameraApp(context: Context, launcher: ActivityResultLauncher<Intent>, uri: Uri) {
     val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        .putExtra(MediaStore.EXTRA_OUTPUT, uri)
 
     if (takePictureIntent.resolveActivity(context.packageManager) != null) {
         launcher.launch(takePictureIntent)
     } else {
         Toast.makeText(context, "Camera App not found.", Toast.LENGTH_SHORT).show()
     }
+}
+
+/**
+ * 保存先ファイルを用意
+ */
+private fun createImageFileUri(context: Context): Uri? {
+    val file = try {
+        val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        File.createTempFile("temp", ".jpg", storageDir)
+    } catch (e: IOException) {
+        return null
+    }
+
+    // 共有可能なcontent URIを取得
+    return FileProvider.getUriForFile(
+        context,
+        "com.omoti.externalcamerasample.fileprovider", // AndroidManifestのProvider名と一致
+        file
+    )
 }
 
 @Preview
